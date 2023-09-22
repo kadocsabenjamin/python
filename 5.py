5c144e51435ebaffcb88b011f50c7cfed83096e2
kadocsabenjamin <kadocsabenjamin@gmail.com>
2023-09-22 10:15:45 +0200

    6.py

--- 5.py
+++ 5.py
@@ -3,6 +3,6 @@ import math
 radius = float(input("Add meg a kör sugarát"))
 
 K = 2 * radius * math.pi
-T = math.pi * radius**2
+T = radius**2 * math.pi 
 
 print(K, "  ", T)
---
+++ 6.py
@@ -0,0 +1,12 @@
+
+nums = [69, 23, 77, 13, 10, 7, 8, 43, 2, 10]
+
+minimum = min(nums)
+maximum = max(nums)
+osszeg =  sum(nums)
+atlag =   osszeg / len(nums)
+
+print(minimum)
+print(maximum)
+print(osszeg)
+print(atlag)
